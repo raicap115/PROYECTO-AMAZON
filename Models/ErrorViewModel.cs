7e9b567ec7847246cfbcfe374445de882b5ec19a
Raí <raicap115@outlook.com>
2022-04-14 14:46:59 -0500

    mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROYECTO_AMAZON.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
